7b16be1e6373f746a2952b39ecdacfa48bfed1fa
Anqi <u7268891@anu.edu.au>
2024-10-16 16:33:56 +1100

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+    return (
+        <div>
+            <div className="container text-center">
+                <div className="row align-items-start">
+                    <div className="col">One of three columns</div>
+                    <div className="col">One of three columns</div>
+                    <div className="col">One of three columns</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
